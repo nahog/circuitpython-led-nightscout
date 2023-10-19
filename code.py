b9b859d85c0a518d7d1fc38fe8209d2bedf5d15e
Nicolas Padula <nahog@msn.com>
2023-10-19 17:56:48 +0100

    Update code.py

--- code.py
+++ code.py
@@ -12,10 +12,13 @@ import socketpool
 import microcontroller
 import adafruit_ntp
 import adafruit_requests
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
 from adafruit_pixel_framebuf import PixelFramebuffer
 from adafruit_datetime import datetime
 
-NIGHTSCOUT_URL="https://<<NIGHTSCOUT_URL>>/api/v1/entries.json?count=1&token=<<NIGHTSCOUT_API_TOKEN>>"
+NIGHTSCOUT_DOMAIN=<<YOUR_NIGHTSCOUT_BASE_DOMAIN>>
+NIGHTSCOUT_TOKEN=<<YOUR_NIGHTSCOUT_API_TOKEN>>
+NIGHTSCOUT_URL="https://" + NIGHTSCOUT_DOMAIN + "/api/v1/entries.json?count=1&token=" + NIGHTSCOUT_TOKEN
 
 URGENT_LOW=3
 LOW=4
@@ -26,13 +29,15 @@ OFF_HOURS_BEGIN=0
 OFF_HOURS_END=8
 TZ_OFFSET=1
 
+# HEX VALUES IN GGRRBB
 OFF_COLOR=0x000000
 BACKGROUND_COLOR=0x000000
-FOREGROUND_COLOR=0x222222
+FOREGROUND_COLOR=0x111111
 URGENT_OUT_OF_RANGE_COLOR=0x00FF00
-OUT_OF_RANGE_COLOR=0xFFFF00
+OUT_OF_RANGE_COLOR=0x49E909
 IN_RANGE_COLOR=0xFF0000
 
+CYCLES_FOR_CLOCK_REFRESH=1800
 UPDATE_CYCLES=16
 TIME_BETWEEN_CYCLES_IN_SEC=2
 
@@ -107,36 +112,48 @@ def print_num(buffer, number):
         buffer.pixel(14, 8, BACKGROUND_COLOR)
     update_screen(buffer)
 
-pool = socketpool.SocketPool(wifi.radio)
-requests = adafruit_requests.Session(pool, ssl.create_default_context())
-ntp = adafruit_ntp.NTP(pool, tz_offset=TZ_OFFSET)
-rtc.RTC().datetime = ntp.datetime
+def update_date(pool):
+    rtc.RTC().datetime = adafruit_ntp.NTP(pool, tz_offset=TZ_OFFSET).datetime
 
-counter = UPDATE_CYCLES
-while True:
-    counter += 1
-    if counter == UPDATE_CYCLES + 1:
-        gc.collect()
-        current_datetime = datetime.now()
-        if current_datetime.hour >= OFF_HOURS_BEGIN and current_datetime.hour <= OFF_HOURS_END:
-            disable_screen = True
-            print(f"night time {current_datetime.hour}:{current_datetime.minute}:{current_datetime.second}, no updates")
+try:
+    pool = socketpool.SocketPool(wifi.radio)
+    requests = adafruit_requests.Session(pool, ssl.create_default_context())
+
+    refresh_time_counter = CYCLES_FOR_CLOCK_REFRESH
+    counter = UPDATE_CYCLES
+    while True:
+        counter += 1
+        refresh_time_counter += 1
+        if refresh_time_counter == CYCLES_FOR_CLOCK_REFRESH + 1:
+            old_datetime = datetime.now()
+            update_date(pool)
+            current_datetime = datetime.now()
+            print(f"time updated from {old_datetime.hour}:{old_datetime.minute:02}:{old_datetime.second:02} to {current_datetime.hour}:{current_datetime.minute:02}:{current_datetime.second:02}")
+            counter = 0
+        if counter == UPDATE_CYCLES + 1:
+            gc.collect()
+            current_datetime = datetime.now()
+            if current_datetime.hour >= OFF_HOURS_BEGIN and current_datetime.hour <= OFF_HOURS_END:
+                disable_screen = True
+                print(f"night time {current_datetime.hour}:{current_datetime.minute:02}:{current_datetime.second:02}, no updates")
+            else:
+                disable_screen = False
+                print(f"getting sgv data on {current_datetime.hour}:{current_datetime.minute:02}:{current_datetime.second:02}...")
+                response = requests.get(NIGHTSCOUT_URL)
+                data = json.loads(response.text)
+                response.close()
+                sgv = round(data[0]["sgv"] / 18.0, 1)
+                print(f"got {sgv}")
+                print_num(buffer=pixel_framebuf, number=str(sgv))
+                pixel_framebuf.line(0, 0, 15, 0, BACKGROUND_COLOR)
+            counter = 0
         else:
-            disable_screen = False
-            print(f"getting sgv data on {current_datetime.hour}:{current_datetime.minute}:{current_datetime.second}...")
-            response = requests.get(NIGHTSCOUT_URL)
-            data = json.loads(response.text)
-            response.close()
-            sgv = round(data[0]["sgv"] / 18.0, 1)
-            print(f"got {sgv}")
-            print_num(buffer=pixel_framebuf, number=str(sgv))
-            pixel_framebuf.line(0, 0, 15, 0, BACKGROUND_COLOR)
-        counter = 0
-    else:
-        print(f"{counter}: waiting 2s")
-        pixel_framebuf.line(0, 0, counter - 1, 0, FOREGROUND_COLOR)
-    time.sleep(TIME_BETWEEN_CYCLES_IN_SEC)
-    update_screen(pixel_framebuf)
+            print(f"{counter}: waiting 2s")
+            pixel_framebuf.line(0, 0, counter - 1, 0, FOREGROUND_COLOR)
+        time.sleep(TIME_BETWEEN_CYCLES_IN_SEC)
+        update_screen(pixel_framebuf)
 
-pixel_framebuf.fill(BACKGROUND_COLOR)
-update_screen(pixel_framebuf)
+    pixel_framebuf.fill(BACKGROUND_COLOR)
+    update_screen(pixel_framebuf)
+except Exception:
+    microcontroller.reset()
